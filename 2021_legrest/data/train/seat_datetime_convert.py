0a58dad11e850e14c96314090c8433d887bb6ec7
Byunghun Hwang <elecun2@gmail.com>
2021-03-10 00:22:58 +0900

    updated format processing

--- 2021_legrest/data/train/seat_datetime_convert.py
+++ 2021_legrest/data/train/seat_datetime_convert.py
@@ -15,23 +15,30 @@ def datetime_convert(target_file):
     #print("Start Datetime(sec) :",ref_datetime[0])
 
     # read seat data
-    seat_data = pd.read_excel(seat_filepath, header=0, index_col=False)
+    seat_data = pd.read_excel(filepath, header=0, index_col=False)
     #fsr_data = pd.read_csv(filepath, header=0, index_col=False)
 
     # get time column and time conversion
-    fsr_data_time_s = fsr_data["Measurement Time (sec)"]+ref_datetime[0]
+    seat_datetime = seat_data["Measurement time"]
+    #print(seat_datetime.values)
+
+    # split string
+    converted_list = [i.split('-') for i in seat_datetime.values]
+    a = ['-'.join(i[0:3])+" "+':'.join(i[3:6])+"."+i[6] for i in converted_list]
+    print(converted_list)
+
 
     # converting to timestamp format
-    converted_datetime = pd.to_datetime(fsr_data_time_s, unit='s')
-    print(converted_datetime)
+    # converted_datetime = pd.to_datetime(fsr_data_time_s, unit='s')
+    # print(converted_datetime)
 
 
     # insert new column for time
-    fsr_data.insert(0, "mtime", converted_datetime, True)
+    # fsr_data.insert(0, "mtime", converted_datetime, True)
     #print(fsr_data)
 
     # write file
-    fsr_data.to_csv("./"+target_file+"-1dm.csv", index=False)
+    # fsr_data.to_csv("./"+target_file+"-1dm.csv", index=False)
 
 
 if __name__ == '__main__':
