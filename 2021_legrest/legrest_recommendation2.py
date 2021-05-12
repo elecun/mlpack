529ffba644569f4186c434a9455cd77f37c5c848
elecun <elecun2@gmail.com>
2021-05-12 23:50:45 +0900

    updated

--- 2021_legrest/legrest_recommendation2.py
+++ 2021_legrest/legrest_recommendation2.py
@@ -161,3 +161,46 @@ target["bmr"] = bmr
 corr = target.corr(method='pearson')
 print('Pearson Correlation Coeff. (relax)\n', corr)
 print('Psearson Correlation :', stats.pearsonr(target['bmr'], target['bestfit_angle_relax']))
+
+
+
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.feature_selection import RFE
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+X = data_config.loc[:, ['user_height', 'user_weight', 'user_age']]
+bmr = 66.47+(13.75*X['user_weight'])+(5*X['user_height'])-(6.76*X['user_age'])
+bmi = X['user_weight']/(X['user_height']/100*X['user_height']/100)
+X['bmr'] = bmr
+X['bmi'] = bmi
+y = data_config.loc[:, ['bestfit_angle_standard']]
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, np.ravel(y), test_size=0.2, random_state=42)
+
+pipelines = []
+pipelines.append(('ScaledLR', Pipeline([('Scaler', StandardScaler()),('LR',LinearRegression())])))
+pipelines.append(('ScaledLASSO', Pipeline([('Scaler', StandardScaler()),('LASSO', Lasso())])))
+pipelines.append(('ScaledEN', Pipeline([('Scaler', StandardScaler()),('EN', ElasticNet())])))
+pipelines.append(('ScaledKNN', Pipeline([('Scaler', StandardScaler()),('KNN', KNeighborsRegressor())])))
+pipelines.append(('ScaledCART', Pipeline([('Scaler', StandardScaler()),('CART', DecisionTreeRegressor())])))
+pipelines.append(('ScaledGBM', Pipeline([('Scaler', StandardScaler()),('GBM', GradientBoostingRegressor())])))
+
+results = []
+names = []
+for name, model in pipelines:
+    kfold = KFold(n_splits=10, random_state=21, shuffle=True)
+    cv_results = cross_val_score(model, X_train, y_train, cv=kfold, scoring='neg_mean_squared_error')
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
