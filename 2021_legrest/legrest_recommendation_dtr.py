a1d861d56d499272aa298b7a93edcfea66cc6faf
elecun <elecun2@gmail.com>
2021-05-28 11:25:48 +0900

    updated

--- 2021_legrest/legrest_recommendation_dtr.py
+++ 2021_legrest/legrest_recommendation_dtr.py
@@ -6,21 +6,10 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import os
-import os.path
-import gc
-from skimage import io, color
-import tensorflow as tf
-from tensorflow.keras.layers import MaxPool1D, GlobalMaxPooling1D
-from sklearn import svm
-from sklearn.datasets._samples_generator import make_blobs
-from tensorflow.keras.layers import MaxPool1D, GlobalMaxPooling1D
 from sklearn.model_selection import train_test_split
-import random
-from sklearn import metrics
-from sklearn.metrics import precision_score
-from scipy import stats
+from sklearn.tree import DecisionTreeRegressor
+import progressbar
+
 
 
 '''
@@ -29,19 +18,11 @@ Presets & Hyper-parameters
 CONFIGURATION_FILE_PATH = "./data/train/data_config.csv"
 DATASET_PATH = "./data/train/"
 pd.set_option('display.width', 200) # for display width
-DYNAMIC_SCALEUP = False
-INTERPOLATION_METHOD = "catrom"
-CASE_PATH = "./catrom_static"
-SAVE_FEATURE_IMAGE = True
-FEATURE_LENGTH = 30 # n-dimensional data feature only use
-NUMBER_OF_SAMPLES = 299 # number of augmented data
-mu, sigma = 0, 1 # normal distribution random parameter for data augmentation
-FEATURE_MAX_LENGTH = 115 # Maximum feature length
-NUMBER_OF_RANDOM_SELECTION = 5
-MAX_TRAIN_ITERATION = -1 # infinity
-SVM_KERNEL_METHOD = 'linear'
-NUMBER_OF_TESTING = 10
-IMAGE_HEIGHT = 369
+# FEATURE_LENGTH = 30 # n-dimensional data feature only use
+# NUMBER_OF_SAMPLES = 299 # number of augmented data
+# FEATURE_MAX_LENGTH = 115 # Maximum feature length
+# NUMBER_OF_RANDOM_SELECTION = 5
+# MAX_TRAIN_ITERATION = -1 # infinity
 
 
 
@@ -54,7 +35,6 @@ data_config = pd.read_csv(CONFIGURATION_FILE_PATH, header=0, index_col=0)
 '''
 2. data extraction
 '''
-from sklearn.tree import DecisionTreeRegressor
 X = data_config.loc[:, ['user_height', 'user_weight', 'user_age']]
 bmr = 66.47+(13.75*X['user_weight'])+(5*X['user_height'])-(6.76*X['user_age'])
 bmi = X['user_weight']/(X['user_height']/100*X['user_height']/100)
@@ -68,32 +48,32 @@ yr = data_config.loc[:, ['bestfit_angle_relax']]
 DecisionTree Regression Model
 '''
 print("------ Regression Model Evaluation (@standard) ------")
-X_train, X_test, y_train, y_test = train_test_split(X, np.ravel(ys), test_size=0.33, shuffle=False)
-model = DecisionTreeRegressor(
+X_train, X_test, y_train, y_test = train_test_split(X, np.ravel(ys), test_size=0.33, shuffle=True)
+model_standard = DecisionTreeRegressor(
     criterion = "mse",
     max_depth=6, 
     min_samples_leaf=1, 
     random_state=1).fit(X_train, y_train)
 
-print("* R2 Score with Trainset (@standard) :", model.score(X_train, y_train))
-print("* R2 Score with Testset (@standard) :", model.score(X_test, y_test))
+print("* R2 Score with Trainset (@standard) :", model_standard.score(X_train, y_train))
+print("* R2 Score with Testset (@standard) :", model_standard.score(X_test, y_test))
 print("* Feature Impotances (@standard) :")
-for name, value in zip(X_train.columns, model.feature_importances_):
+for name, value in zip(X_train.columns, model_standard.feature_importances_):
     print('  - {0}: {1:.3f}'.format(name, value))
 
 
 print("------ Regression Model Evaluation (@relax) ------")
-X_train, X_test, y_train, y_test = train_test_split(X, np.ravel(yr), test_size=0.33, shuffle=False)
-model = DecisionTreeRegressor(
+X_train, X_test, y_train, y_test = train_test_split(X, np.ravel(yr), test_size=0.33, shuffle=True)
+model_relax = DecisionTreeRegressor(
     criterion = "mse", # mean square error
     max_depth=6, 
     min_samples_leaf=1, 
     random_state=1).fit(X_train, y_train)
 
-print("* R-squared Score with Trainset (@relax) :", model.score(X_train, y_train))
-print("* R-squared Score with Testset (@relax) :", model.score(X_test, y_test))
+print("* R-squared Score with Trainset (@relax) :", model_relax.score(X_train, y_train))
+print("* R-squared Score with Testset (@relax) :", model_relax.score(X_test, y_test))
 print("* Feature Impotances (@standard) :")
-for name, value in zip(X_train.columns, model.feature_importances_):
+for name, value in zip(X_train.columns, model_relax.feature_importances_):
     print('  - {0}: {1:.3f}'.format(name, value))
 
 '''
@@ -111,8 +91,24 @@ min_weight = 40
 max_weight = 100
 weights = np.array([min_weight+i for i in range(max_weight-min_weight+1)])
 
+bar = progressbar.ProgressBar(maxval=len(ages)*len(heights)*len(weights), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+bar.start()
+output_standard = pd.DataFrame(columns=['age','height','weight','legrest'])
+output_relax = pd.DataFrame(columns=['age','height','weight','legrest'])
+count = 0
 for a in ages:
     for h in heights:
         for w in weights:
+            bmr = 66.47+(13.75*w)+(5*h)-(6.76*a)
+            bmi = w/(h/100*h/100)
+            pvs = model_standard.predict([[a,h,w,bmr,bmi]])
+            pvr = model_relax.predict([[a,h,w,bmr,bmi]])
+            output_standard = output_standard.append({'age':a, 'height':h, 'weight':w, 'legrest':pvs}, ignore_index=True)
+            output_relax = output_relax.append({'age':a, 'height':h, 'weight':w, 'legrest':pvr}, ignore_index=True)
+            count = count+1
+            bar.update(count)
+bar.finish()
 
-
+output_standard.to_csv('result_standard.csv')
+output_relax.to_csv('result_relax.csv')
+print("saved results")
